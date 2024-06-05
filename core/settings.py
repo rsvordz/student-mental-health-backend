4e944a3d5588f4b3d7fe2d5850d25e7c9c1ee608
rsvordz <rubenvordzorgbe@gmail.com>
2024-06-05 18:19:36 +0000

    work

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -144,7 +145,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 
 
 REST_FRAMEWORK = {
